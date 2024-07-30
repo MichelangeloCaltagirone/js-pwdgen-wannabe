f94231337883fd7d8869523e362bbab0deffbe37
MichelangeloCaltagirone <michelangelocaltagirone@gmail.com>
2024-07-30 16:44:17 +0200

    Exercise Finished. Add comments, done some tests for the multiple way of showing the result in page.

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-console.log("it's alive!")
-
 /* 
 TRACCIA
 Chiedi all’utente il suo nome,
@@ -10,42 +8,66 @@ Infine stampa in console nomecognomecolorepreferito21
 
 // Fase preparatoria
 /*-prendo l'elemento dal DOM che mi interessa per lavorare,
-in questo caso un tag <span>.
+in questo caso un tag <div>.
 */
 // Fase raccolta dati
-/*
-Chiedo all'utente, tramite prompt, di inserire i 3 dati richiesti
+/*-Chiedo all'utente, tramite prompt, di inserire i 3 dati richiesti
 (nome, cognome, colore preferito) precompilandoli già, e assegnandoli
 a variabili (costanti).
 */
 // Fase di lavorazione dati 
-/*
+/*-approcio#:creo una variabile let(poi modificabile) e unisco
+le const chieste all'utente
+-approcio# creo una const dove sommo assieme tutte le stringhe che mi servono
+(le 3 date da input dall'Utente, e il suffiso '21', scelto arbitrariamente)
 */
 // Fase di produzione di output
-/*
+/* -approcio#:-cambio il contenuto testuale dell'elemento in pagina con 
+la variabile 'somma' creata in lavorazione dati, e gli 'monto'
+direttamente il suffisso '21'.
+-approcio#:-cambio il contenuto dell'HTML dell'elemento che mi interessa in pagina, 
+inserendo una stringa contenente la variabile const (messa in risalto da <strong>)
 */
 
 
 // Fase Prep
-const resultDiv = document.getElementById('result');
-console.log(resultDiv);
+//recupero l'unico elemento di interesse nella pagina html, tramite Id
+const resultElement = document.getElementById('result');
 
 
 // Fase Racc. Dati
-const fisrtName = prompt('Digita il tuo Nome', 'Michelangelo');
+// Chiedo all'utente i dati richiesti, tramite prompt, e li assegno a delle const
+const fisrtName = prompt('Digita il tuo Nome', 'michelangelo');
 console.log(fisrtName);
-const lasttName = prompt('Digita il tuo Cognome', 'Caltagirone');
+const lasttName = prompt('Digita il tuo Cognome', 'caltagirone');
 console.log(lasttName);
-const favCol = prompt('Digita il tuo colore preferito', 'Viola');
+const favCol = prompt('Digita il tuo colore preferito', 'viola');
 console.log(favCol);
 
 
 // Fase lavorazioni dati
 
+/*
+// creo una variabile let sommando le const create con i dati dell'utente
 let secretPwd = fisrtName + lasttName + favCol;
+console.log(secretPwd);
+*/
+//metodo alternativo
+// creo una const, sommando direttamente tutte le stringhe necessarie
+const secretPwd = fisrtName + lasttName + favCol + '21';
 console.log(secretPwd);
 
 
 // Fase prod. output
 
-resultDiv.innerText = secretPwd +='21';
+// 'Monto' direttamente il suffisso richiesto nel contenuto testuale dell'elemento in pagina
+// resultElement.innerText = secretPwd +='21';
+
+// metodo alternativo
+/* Mostro la const creata prima in pagina, già compresa di suffisso, con dei backtick per accompagnarla con del testo (cambio solo il
+contenuto testuale)*/
+//resultElement.innerText = `La tua password super segreta è: ${secretPwd}, salvala mi raccomando!`;
+console.log(resultElement);
+// Mostro la const creata prima, inserendola direttamente nel HTML dell'elemento in pagina.
+resultElement.innerHTML = `La tua super password segreta è: <strong>${secretPwd}</strong> , salvala mi raccomando!!`;
+console.log(resultElement);
